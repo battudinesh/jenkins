480d785bf73c000058f385843d8eece8077abfc8
battudinesh <battudineshkumar123@gmail.com>
2026-01-28 17:51:57 +0530

    Test webhook wellcome2

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ app=FastAPI(title="this neww app")
 @app.get('/')
 async def wellcome():
     return {"data":"this is firast data"}
+@app.get('/ss')
+async def wellcome2():
+    return {"data":"this is firast data2"}
