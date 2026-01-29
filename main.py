4f967d280b9df1263c9ded04ad7412ad69beac20
battudinesh <battudineshkumar123@gmail.com>
2026-01-29 06:14:30 +0530

    Test webhook auto-build4

--- main.py
+++ main.py
@@ -4,6 +4,3 @@ app=FastAPI(title="this neww app")
 @app.get('/')
 async def wellcome():
     return {"data":"this is firast data"}
-@app.get('/ss')
-async def wellcome2():
-    return {"data":"this is firast data2"}
